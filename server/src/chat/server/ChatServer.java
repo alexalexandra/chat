629d1cabcc97837fe0016dbcb28a0597d90274c1
hitech <sas9146@bk.ru>
2019-01-04 04:46:15 +0300

    сервер готов

--- server/src/chat/server/ChatServer.java
+++ server/src/chat/server/ChatServer.java
@@ -3,15 +3,19 @@ package chat.server;
 
 import chat.network.TCPConnection;
 import chat.network.TCPConnectionListener;
+import sun.awt.SunHints;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 public class ChatServer implements TCPConnectionListener {
 
     public static void main(String[] args) {
      new ChatServer();
     }
+     //список соединений
+    private final ArrayList<TCPConnection> connections = new ArrayList<>();
 
     private  ChatServer()  {
        System.out.println("сервер запущен");
@@ -30,24 +34,35 @@ public class ChatServer implements TCPConnectionListener {
             throw new RuntimeException(e);
        }
     }
+//обработка событий на сервере. синх-м,т.к.мб много потоков
+    @Override
+        public synchronized void onConnectionReady(TCPConnection topConnection) {
+            connections.add(topConnection);//добавляем соединение
+        //оповестили всех
+            sendToAllConnections("Соединение установлено:"+ topConnection);
+        }
 
     @Override
-    public void onConnectionReady(TCPConnection topConnection) {
-
+    public synchronized void onReceiveString(TCPConnection topConnection, String value) {
+        //если приняли строчку
+        sendToAllConnections(value);
     }
 
     @Override
-    public void onReceiveString(TCPConnection topConnection, String value) {
-
+    public synchronized void onDisconnect(TCPConnection topConnection) {
+       connections.remove(topConnection); //если соедин-е отвалилось - удаляем
+        sendToAllConnections("Соединение прервано:"+ topConnection);
     }
 
     @Override
-    public void onDisconnect(TCPConnection topConnection) {
-
+    public synchronized void onException(TCPConnection topConnection, Exception e) {
+        System.out.println("TCPConnection exception: "+ e); //исключение
     }
 
-    @Override
-    public void onException(TCPConnection topConnection, Exception e) {
-
+    private void sendToAllConnections(String value) {  //отправляет сообщение всем
+        System.out.println(value);
+        for (int i=0; i<connections.size();i++){
+            connections.get(i).sendString(value);
+        }
     }
 }
